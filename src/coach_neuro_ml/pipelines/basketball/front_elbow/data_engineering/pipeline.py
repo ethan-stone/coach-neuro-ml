ebba0ac085ea756661ef2d2d3c343c2e5aad76cc
ethan-stone <ethan.stone9352@gmail.com>
2021-02-10 00:57:22 -0500

    migrating to gcp

--- src/coach_neuro_ml/pipelines/basketball/front_elbow/data_engineering/pipeline.py
+++ src/coach_neuro_ml/pipelines/basketball/front_elbow/data_engineering/pipeline.py
@@ -6,8 +6,8 @@ def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(func=process_raw_data,
-                 inputs="aws_basketball_front_elbow_raw",
-                 outputs="aws_basketball_front_elbow_primary",
+                 inputs="gcs_basketball_front_elbow_raw",
+                 outputs="gcs_basketball_front_elbow_primary",
                  name="process_basketball_front_elbow_raw")
         ]
     )
--- src/coach_neuro_ml/pipelines/basketball/front_elbow/model_building/pipeline.py
+++ src/coach_neuro_ml/pipelines/basketball/front_elbow/model_building/pipeline.py
@@ -6,7 +6,7 @@ def create_pipeline():
     return Pipeline(
         [
             node(func=split_data,
-                 inputs=["aws_basketball_front_elbow_primary", "parameters"],
+                 inputs=["gcs_basketball_front_elbow_primary", "parameters"],
                  outputs=["X_train_basketball_front_elbow",
                           "X_test_basketball_front_elbow",
                           "y_train_basketball_front_elbow",
@@ -15,10 +15,10 @@ def create_pipeline():
             node(func=train_model,
                  inputs=["X_train_basketball_front_elbow",
                          "y_train_basketball_front_elbow"],
-                 outputs="aws_basketball_front_elbow_model",
+                 outputs="gcs_basketball_front_elbow_model",
                  name="train_basketball_front_elbow_model"),
             node(func=evaluate_model,
-                 inputs=["aws_basketball_front_elbow_model",
+                 inputs=["gcs_basketball_front_elbow_model",
                          "X_test_basketball_front_elbow",
                          "y_test_basketball_front_elbow"],
                  outputs=None,
